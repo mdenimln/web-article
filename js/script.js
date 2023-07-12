bfb2a40bb1261786b19a258402a6905866fb00dc
Deni Maulana <denimlnm@gmail.com>
2023-07-12 21:40:02 +0700

    add fungsi efek sticky in header

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
-const tombolMenu = document.querySelector('.menu-mobile');
-const back = document.querySelector('.back');
-const navigasi = document.querySelector('.nav-list');
-const aktip = document.querySelectorAll('.active');
+const tombolMenu = document.querySelector(".menu-mobile");
+const back = document.querySelector(".back");
+const navigasi = document.querySelector(".nav-list");
+const aktip = document.querySelectorAll(".active");
 window.onload = () => {
-    tombolMenu.onclick = () => {
-       navigasi.classList.add('arise')
-       
-    }
-    aktip.forEach((item) => {
-        item.onclick = () => {
-            navigasi.classList.remove('arise')
-        }
-    })
-    back.onclick = () => {
-        navigasi.classList.remove('arise')
-    }
-    // Smooth ketika scroll otomatis
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e){
-            e.preventDefault()
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior : "smooth"
-            })
-        })
-    }) 
-  }
+  tombolMenu.onclick = () => {
+    navigasi.classList.add("arise");
+  };
+  aktip.forEach((item) => {
+    item.onclick = () => {
+      navigasi.classList.remove("arise");
+    };
+  });
+  back.onclick = () => {
+    navigasi.classList.remove("arise");
+  };
+  // Smooth ketika scroll otomatis
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+
+  const headerSticky = document.querySelector("header");
+  window.addEventListener("scroll", function () {
+    headerSticky.classList.toggle("sticky", window.scrollY > 50);
+  });
+};
